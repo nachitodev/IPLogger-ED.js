cbabef31ef1bc543a83bfc982db714c0c9a35029
in the moon <88955484+uhIgnacio@users.noreply.github.com>
2021-09-05 16:12:23 -0300

    beautify and space

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const events = require("events"),
-	express = require("express"),
-	cors = require("cors"),
-	{ Webhook } = require("discord-webhook-node"),
-	config = require("./config.json"),
-	hook = new Webhook(config.webhook),
-	app = express()
+    express = require("express"),
+    cors = require("cors"),
+    {
+        Webhook
+    } = require("discord-webhook-node"),
+    config = require("./config.json"),
+    hook = new Webhook(config.webhook),
+    app = express()
 
 app.use(cors())
 
@@ -12,16 +14,16 @@ app.use(cors())
 const client = new events.EventEmitter()
 
 app.use("/", (req, res) => {
-	const user_ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
-	const text = `${user_ip} at ${new Date()}`
-	client.emit("NewIp", text);
+    const user_ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    const text = `${user_ip} at ${new Date()}`
+    client.emit("NewIp", text);
 });
 
 
 client.on("NewIp", text => {
-	hook.send(text);
+    hook.send(text);
 })
 
 
 app.listen(config.port);
-console.log(`IPLogger Running in port ${config.port}`)
+console.log(`IP Logger Running in port ${config.port}`)
